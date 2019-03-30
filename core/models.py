abd3bb0fa2cf1e269f59f94fcf23887decfb40d8
Данила <guseffdanila@ya.ru>
2019-03-30 15:41:06 +0300

    Add Document models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class DocumentSupertype(models.Model):
+    name = models.CharField("name od supertype", max_length=45, unique=True)
+
+
+class Owner(models.Model):
+    pk = models.IntegerField(primary_key=True)
+
+
+class Document(models.Model):
+    doc_id = models.CharField("id of document", max_length=45, unique=True)
+    text = models.TextField("source of document")
+    type = models.ForeignKey(DocumentSupertype, on_delete=models.PROTECT)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    is_visible = models.BooleanField(default=True)
