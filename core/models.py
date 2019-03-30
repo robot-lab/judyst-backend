300723a95a58f256ed92830eb9d172e207d195a2
Данила <guseffdanila@ya.ru>
2019-03-30 16:21:13 +0300

    Add user models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -29,3 +30,45 @@ class Analyzer(models.Model):
 
     class Meta:
         unique_together = ('version', 'name',)
+
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_activated = models.BooleanField(default=False)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+
+    @classmethod
+    def create(cls, **kwargs):
+        if 'owner' in kwargs:
+            del kwargs['owner']
+        user = cls(owner=Owner.objects.create(), **kwargs)
+        user.save()
+        return user
+
+    def __str__(self):
+        return self.email
+
+
+class Organisation(models.Model):
+    name = models.CharField(max_length=150)
+    registration_date = models.DateField(auto_now=True)
+    is_activated = models.BooleanField(default=False)
+    members = models.ManyToManyField(CustomUser, through='OrganisationUser',
+                                     through_fields=('organisation', 'user'))
+
+    @classmethod
+    def create(cls, **kwargs):
+        if 'owner' in kwargs:
+            del kwargs['owner']
+        org = cls(owner=Owner.objects.create(), **kwargs)
+        org.save()
+        return org
+
+
+class OrganisationUser(models.Model):
+    organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('organisation', 'user',)
