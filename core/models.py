a84ed0bd99abb669976c0d64ff40b68f0b6dc835
Данила <guseffdanila@ya.ru>
2019-03-30 15:54:04 +0300

    Add analyzers models

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class DocumentSupertype(models.Model):
-    name = models.CharField("name od supertype", max_length=45, unique=True)
+    name = models.CharField("name of supertype", max_length=45, unique=True)
 
 
 class Owner(models.Model):
@@ -15,3 +15,17 @@ class Document(models.Model):
     type = models.ForeignKey(DocumentSupertype, on_delete=models.PROTECT)
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
     is_visible = models.BooleanField(default=True)
+
+
+class AnalyzerType(models.Model):
+    name = models.CharField("name of analyze, which provide these analyzers",
+                            max_length=100, unique=True)
+
+
+class Analyzer(models.Model):
+    version = models.IntegerField("version of current analyzer")
+    name = models.CharField("name of analyzer", max_length=100)
+    analyzer_type = models.ForeignKey(AnalyzerType, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = ('version', 'name',)
